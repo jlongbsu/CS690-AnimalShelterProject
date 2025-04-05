f3232fa3b072874bba7d6e842d542d36a30a2049
jlongbsu <joseph.long@bsu.edu>
2025-04-05 06:34:33 +0000

    Setup testing

---
+++ AnimalShelterProject/AnimalShelter.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace AnimalShelter.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
--- AnimalShelterProject/AnimalShelter/Program.cs
+++ AnimalShelterProject/AnimalShelter/Program.cs
@@ -1,9 +1,26 @@
 ﻿namespace AnimalShelter;
 
+using System.IO;
+using System;
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("What would you like to do?");
+        string mode = Console.ReadLine();
+
+        if(mode == "add"){
+            Console.WriteLine("Enter the animal's name: ");
+            string animalName = Console.ReadLine();
+            Console.WriteLine("Enter the animal's type: ");
+            string animalType = Console.ReadLine();
+            Console.WriteLine("Enter the animal's age: ");
+            int animalAge = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the animal's adoption status: ");
+            string animalAdoptionStatus = Console.ReadLine();
+
+            File.AppendAllText("animals.txt", $"{animalName},{animalType},{animalAge},{animalAdoptionStatus},{DateTime.Today.ToString("d")}{Environment.NewLine}");
+        }
     }
 }
