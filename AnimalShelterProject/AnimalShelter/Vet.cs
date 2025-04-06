28f32531f0ed528648a49be0649e8748fddeac87
jlongbsu <joseph.long@bsu.edu>
2025-04-06 08:53:16 +0000

    Added classes from domain model. Mostly focused on Animal class. Started implementing functionality using Spectre.

---
+++ AnimalShelterProject/AnimalShelter/Vet.cs
@@ -0,0 +1,10 @@
+namespace AnimalShelter;
+
+public class Vet{
+    public string Name {get;}
+    public string Address {get;}
+    public Vet(string name, string address){
+        this.Name = name;
+        this.Address = address;
+    }
+}
